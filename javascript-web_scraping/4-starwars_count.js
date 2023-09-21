2e4caf81a8e4d80f6760d58fc7ccfd86455e2591
Patricio-Benglian <6305@holbertonstudents.com>
2023-09-21 12:24:13 -0700

    Nested l00ps

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (!err) {
+    const listOfFilms = JSON.parse(body);
+
+    let filmsWithWedge = 0;
+    listOfFilms.results.forEach(film => {
+      film.characters.forEach(chars => {
+        if (chars.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+          filmsWithWedge += 1;
+        }
+      });
+    });
+    console.log(filmsWithWedge);
+  }
+});
