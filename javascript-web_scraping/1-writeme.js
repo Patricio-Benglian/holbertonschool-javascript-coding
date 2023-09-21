d30b9dc9728fada660c3e18b6c86f97f2b49e300
Patricio-Benglian <6305@holbertonstudents.com>
2023-09-21 11:32:11 -0700

    semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const fileContent = process.argv[3];
 
-fs.writeFile(`${fileName}`, fileContent, 'utf8', function(err) {
+fs.writeFile(`${fileName}`, fileContent, 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
-})
+});
